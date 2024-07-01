9e1e4436962759b2adeaadc872b7c34769c0e24d
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-01 10:20:22 +0100

    push

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 const express = require("express")
 const axios = require("axios")
+require('dotenv').config();
+
 
 const app = express();
-const PORT = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 app.get("/api/hello", async (req, res) => {
-    const visitor = req.query.visitor
+const visitor = req.query.visitor
 const clientIp = req.ip
 
 try {
     const locationResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
     const locationData = locationResponse.data 
     const city = locationData.city 
-    const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a84ad6ad0eaf0a2392b953262ba97508`);
+    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+    const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
     const weatherData = weatherResponse.data;
     const Temperature = weatherData.main.temp;
 
@@ -30,7 +33,9 @@ try {
     res.status(500).send('internal Server Error')
     
 }
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
 
+})
 
-}
-)
