25978cb259e47128bac3553dfe9f1a1a96fc3538
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-01 12:55:17 +0100

    edit

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+app.get('/', (req, res)=>{
+    res.send("nav to '/api/hello?visitor_name' ")
+})
+
+
 app.get('/api/hello', async (req, res) => {
     const visitorName = req.query.visitor_name || 'Guest';
     const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
