c6eb47434cca530b8c5b15008ea0bd946a385647
Muyiwa Obaremi <obaremimuyiwa@gmail.com>
2024-07-01 11:07:56 +0100

    update

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-const express = require("express")
-const axios = require("axios")
-require('dotenv').config();
-
-
-const app = express();
-const port = process.env.PORT || 3000
-
 app.get("/api/hello", async (req, res) => {
-const visitor = req.query.visitor
-const clientIp = req.ip
+    const visitor = req.query.visitor;
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-try {
-    const locationResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
-    const locationData = locationResponse.data 
-    const city = locationData.city 
-    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-    const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
-    const weatherData = weatherResponse.data;
-    const Temperature = weatherData.main.temp;
+    try {
+        console.log('Client IP:', clientIp);
+        const locationResponse = await axios.get(`http://ip-api.com/json/${clientIp}`);
+        const locationData = locationResponse.data;
+        console.log('Location data:', locationData);
 
-    const response = {
-        client_IpAddress : clientIp,
-        location : locationData,
-        Temperature : Temperature,
-       greeting: `Hello, ${visitor}! The temperature is ${Temperature} degrees Celsius in ${city}`
+        const city = locationData.city;
+        console.log('City:', city);
+
+        if (!city) {
+            throw new Error('Unable to determine city from IP address');
+        }
+
+        const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+        const weatherResponse = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`);
+        const weatherData = weatherResponse.data;
+        const temperature = weatherData.main.temp;
+
+        const response = {
+            client_IpAddress: clientIp,
+            location: locationData,
+            temperature: temperature,
+            greeting: `Hello, ${visitor}! The temperature is ${temperature} degrees Celsius in ${city}`
+        };
+
+        res.json(response);
+    } catch (error) {
+        console.error('Error details:', error.message);
+        if (error.response) {
+            console.error('Error response:', error.response.data);
+        }
+        res.status(500).send('Internal Server Error');
     }
-
-    res.json(response)
-
-} catch (error) {
-    console.error(error)
-    res.status(500).send('internal Server Error')
-    
-}
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-
-})
-
+});
